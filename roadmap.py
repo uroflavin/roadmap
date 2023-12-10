e0727e0a167111f6f3460244020fbca69b905658
Uroflavin <>
2023-12-10 22:20:41 +0100

    jinja2 template

--- roadmap.py
+++ roadmap.py
@@ -11,15 +11,19 @@ from jinja2 import Template
 LOGFILE= os. getcwd() + "/roadmap.log"
 logging.basicConfig(filename=LOGFILE, encoding='utf-8', level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-config = dotenv_values(".env") 
+# No need already
+# config = dotenv_values(".env") 
 
+## Read Example
 with open('examples/roadmap.full.yml') as f:
     project = yaml.load(f, Loader=yaml.FullLoader)
     logging.debug("%s", project)
 
+## Read Markdown Template
 with open('roadmap.template.md') as f:
     tmpl = Template(f.read())
-output_from_parsed_template = tmpl.render(project = project)
+    output_from_parsed_template = tmpl.render(project = project)
 
+## Render Markdown
 with open("roadmap.md", "w") as f:
     f.write(output_from_parsed_template)
