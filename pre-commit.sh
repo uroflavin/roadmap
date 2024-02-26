1a5a1c4fdf6832307ab386b6522b477a459a1b81
Uroflavin <uroflavin@gmail.com>
2024-02-26 18:51:46 +0100

    feat(dev): added rebuild of requirements as pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 jsonschema2md --examples-as-yaml --show-examples all schema/roadmap.json schema/roadmap.md
 git add schema/roadmap.md
+pip freeze > requirements.txt
+git add requirements.txt
 #git commit --amend -C HEAD
